3d8f5d52d325d6dd6712efc19642f67216e0098e
Juan Diego Osorio <jd.osorioc1@uniandes.edu.co>
2024-04-27 22:08:27 -0500

    cambios

--- P2.py
+++ P2.py
@@ -39,6 +39,8 @@ def dijkstra(graph, start, end):
     return float("inf"), []
 
 def p2 (elementos: list,w1,w2):
+
+
     atomoosLibres = []
     for elemento in elementos:
         for atomoo in elemento:
@@ -54,13 +56,43 @@ def p2 (elementos: list,w1,w2):
             if atomo != vecino and atomo*-1 != vecino:
                 graph[atomo][vecino] = calculate_LTP(atomo,vecino, w1,w2)
 
-    return atomoosLibres, graph
+
+    resultados = []
+    #se itera por cada elemento de la lista de elementos
+    for elemento in elementos:
+        #se crea una copia de la lista de elementos eliminando el elemento a revisar
+        elementos.remove(elemento)
+        copia = elementos.copy()
+        #por cada atomo del elememnto revisando
+        for atomo in elemento:
+            #se itera cada elemento de la copia
+            for elementico in copia:
+                #se revisa si el atomo del elemento que estamos revisando se encuentra en alguno otro elemento
+                if atomo in elementico:
+                    #si esta entonces se llama al dijsktra para hallar el camino mas corto para conectar los elementos
+                    energia,caminoMinimo = dijkstra(graph,atomo,atomo*-1)
+
+                    #revisamos si estan en la misma posicion en sus listas
+                    if elemento.index(atomo) == elementico.index(atomo):
+                        nuevaLista = []
+                        if elementico.index(atomo) == 1:
+                            nuevaLista.append(atomo)
+                            nuevaLista.append(elementico[0])
+                        else:
+                            nuevaLista.append(elementico[1])
+                            nuevaLista.append(atomo)
+                        resultado = (elemento,caminoMinimo,nuevaLista,energia)
+                        resultados.append(resultado)
+    return resultados
+    #return atomoosLibres, graph
 
 # Ejemplo de uso
-atomos, graph = p2([[1,3],[-6,3],[1,7]], 3, 5)
-print("Graph:", graph)
-start_node = 1
-end_node = -1
-min_distance, path = dijkstra(graph, start_node, end_node)
-print("Distancia mínima:", min_distance)
-print("Camino:", path)
+#atomos, graph = p2([[1,3],[-6,3],[1,7]], 3, 5)
+#print("Graph:", graph)
+#start_node = 1
+#end_node = -1
+#min_distance, path = dijkstra(graph, start_node, end_node)
+#print("Distancia mínima:", min_distance)
+#print("Camino:", path)
+print(p2([[1,3],[-6,3],[1,7]],3,5))
+
